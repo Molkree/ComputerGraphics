23bd6df84b5a79eeecb79ef7751480263eff39cb
Dmitry Volodin <mr.molkree@gmail.com>
2018-09-11 17:55:07 +0300

    Different channels - done

--- Lab2_task2/Form1.cs
+++ Lab2_task2/Form1.cs
@@ -21,44 +21,53 @@ namespace Lab2_task2
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
-            Graphics g = Graphics.FromImage(pictureBox1.Image);
-            Bitmap bmp = pictureBox1.Image as Bitmap;
+            pictureBox2.Image = Image.FromFile(openFileDialog1.FileName);
+            pictureBox3.Image = Image.FromFile(openFileDialog1.FileName);
+            Bitmap bmp1 = pictureBox1.Image as Bitmap;
+            Bitmap bmp2 = pictureBox2.Image as Bitmap;
+            Bitmap bmp3 = pictureBox3.Image as Bitmap;
 
             // Lock the bitmap's bits.  
-            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            Rectangle rect = new Rectangle(0, 0, bmp1.Width, bmp1.Height);
             System.Drawing.Imaging.BitmapData bmpData =
-                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bmp.PixelFormat);
+                bmp1.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp1.PixelFormat);
 
             // Get the address of the first line.
             IntPtr ptr = bmpData.Scan0;
 
             // Declare an array to hold the bytes of the bitmap.
-            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            int bytes = Math.Abs(bmpData.Stride) * bmp1.Height;
             rgbValues = new byte[bytes];
 
             // Copy the RGB values into the array.
             System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
 
-            // Set every third value to 255. A 24bpp bitmap will look red.  
-            for (int counter = 0; counter < rgbValues.Length; counter += 3)
-            {
-                //rgbValues[counter + 1] = 255;                                
-            }
-            // Copy the RGB values back to the bitmap
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            //// Set every third value to 255. A 24bpp bitmap will look red.  
+            //for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            //{
+            //    //rgbValues[counter + 1] = 255;                                
+            //}
+            //// Copy the RGB values back to the bitmap
+            //System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
 
             // Unlock the bits.
-            bmp.UnlockBits(bmpData);
+            bmp1.UnlockBits(bmpData);
 
             int i = 0;
             for (int counter = 0; counter < rgbValues.Length; counter += 3)
             {
-                bmp.SetPixel(i % bmp.Width, i / bmp.Width,
-                    Color.FromArgb(/*rgbValues[counter + 2]*/0, /*rgbValues[counter + 1]*/0, rgbValues[counter]));
+                bmp1.SetPixel(i % bmp1.Width, i / bmp1.Width,
+                    Color.FromArgb(rgbValues[counter + 2], 0, 0));
+                bmp2.SetPixel(i % bmp1.Width, i / bmp1.Width,
+                    Color.FromArgb(0, rgbValues[counter + 1], 0));
+                bmp3.SetPixel(i % bmp1.Width, i / bmp1.Width,
+                    Color.FromArgb(0, 0, rgbValues[counter]));
                 i++;
             }
             pictureBox1.Refresh();
+            pictureBox2.Refresh();
+            pictureBox3.Refresh();
         }
     }
 }
