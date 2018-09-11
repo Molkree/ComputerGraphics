416383b76416818bd81242cfaf2496b0a5e2c01b
Taally <shkuro_tanya@mail.ru>
2018-09-12 00:05:56 +0300

    task3 added

---
+++ Lab2_task3/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Lab2_task3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(134, 84);
+			this.trackBar1.Maximum = 100;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(142, 45);
+			this.trackBar1.TabIndex = 2;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.Location = new System.Drawing.Point(134, 135);
+			this.trackBar2.Maximum = 100;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(142, 45);
+			this.trackBar2.TabIndex = 3;
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.Location = new System.Drawing.Point(134, 186);
+			this.trackBar3.Maximum = 100;
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Size = new System.Drawing.Size(142, 45);
+			this.trackBar3.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(26, 84);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(76, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Hue (оттенок)";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(26, 135);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(102, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Saturation (насыщ.)";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(26, 186);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(84, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Value (яркость)";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(29, 35);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(142, 23);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Выбрать изображение";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(348, 35);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(331, 260);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 9;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(29, 237);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(142, 23);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "Сохранить изображение";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(691, 315);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.trackBar3);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.trackBar1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ Lab2_task3/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_task3
+{
+	public partial class Form1 : Form
+	{
+	//	String path;
+		byte[] rgbValues;
+		double hue, sat, val;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+				if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+				//	System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+				//	sr.Close();
+				//	path = openFileDialog1.FileName;
+				//	pictureBox1.Image(
+				}
+		}
+
+		// copypaste ----
+		public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+		{
+			int max = Math.Max(color.R, Math.Max(color.G, color.B));
+			int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+			hue = color.GetHue();
+			saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+			value = max / 255d;
+		}
+
+		public static Color ColorFromHSV(double hue, double saturation, double value)
+		{
+			int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+			double f = hue / 60 - Math.Floor(hue / 60);
+
+			value = value * 255;
+			int v = Convert.ToInt32(value);
+			int p = Convert.ToInt32(value * (1 - saturation));
+			int q = Convert.ToInt32(value * (1 - f * saturation));
+			int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+			if (hi == 0)
+				return Color.FromArgb(255, v, t, p);
+			else if (hi == 1)
+				return Color.FromArgb(255, q, v, p);
+			else if (hi == 2)
+				return Color.FromArgb(255, p, v, t);
+			else if (hi == 3)
+				return Color.FromArgb(255, p, q, v);
+			else if (hi == 4)
+				return Color.FromArgb(255, t, p, v);
+			else
+				return Color.FromArgb(255, v, p, q);
+		}
+		// ------------
+
+		private void showImage(double hue, double sat, double val)
+		{
+			pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+			Graphics g = Graphics.FromImage(pictureBox1.Image);
+			Bitmap bmp = pictureBox1.Image as Bitmap;
+
+			// Lock the bitmap's bits.  
+			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+
+			System.Drawing.Imaging.BitmapData bmpData =
+				bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+				bmp.PixelFormat);
+
+			// Get the address of the first line.
+			IntPtr ptr = bmpData.Scan0;
+
+			// Declare an array to hold the bytes of the bitmap.
+			int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+			rgbValues = new byte[bytes];
+
+			// Copy the RGB values into the array.
+			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+			// Set every third value to 255. A 24bpp bitmap will look red.  
+			for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			{
+				//rgbValues[counter+1] = 255;                                
+			}
+			// Copy the RGB values back to the bitmap
+			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+			// Unlock the bits.
+			bmp.UnlockBits(bmpData);
+
+			int i = 0;
+			for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			{
+				/*				int red, green, blue;
+								red = rgbValues[counter + 2];
+								green = rgbValues[counter + 1];
+								blue = rgbValues[counter];
+								*/
+
+				Color orig = Color.FromArgb(rgbValues[counter + 2], rgbValues[counter + 1], rgbValues[counter]);
+
+				//		hue = orig.GetHue();
+				//		sat = orig.GetSaturation();
+				//		val = orig.GetBrightness();
+
+				//		bmp.SetPixel(i % bmp.Width, i / bmp.Width, orig);
+
+				//	ColorToHSV(orig, out hue, out sat, out val);
+
+
+
+				Color newcolor = ColorFromHSV(hue, sat, val);
+
+				bmp.SetPixel(i % bmp.Width, i / bmp.Width, newcolor);
+
+
+				i++;
+
+			}
+			pictureBox1.Refresh();
+		}
+
+
+		private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+		{
+			pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+			Graphics g = Graphics.FromImage(pictureBox1.Image);
+			Bitmap bmp = pictureBox1.Image as Bitmap;
+
+			// Lock the bitmap's bits.  
+			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+
+			System.Drawing.Imaging.BitmapData bmpData =
+				bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+				bmp.PixelFormat);
+
+			// Get the address of the first line.
+			IntPtr ptr = bmpData.Scan0;
+
+			// Declare an array to hold the bytes of the bitmap.
+			int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+			rgbValues = new byte[bytes];
+
+			// Copy the RGB values into the array.
+			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+			// Copy the RGB values back to the bitmap
+			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+			// Unlock the bits.
+			bmp.UnlockBits(bmpData);
+
+			int i = 0;
+			for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			{
+				Color orig = Color.FromArgb(rgbValues[counter + 2], rgbValues[counter + 1], rgbValues[counter]);
+
+				//		hue = orig.GetHue();
+				//		sat = orig.GetSaturation();
+				//		val = orig.GetBrightness();
+
+				//		bmp.SetPixel(i % bmp.Width, i / bmp.Width, orig);
+
+				//	ColorToHSV(orig, out hue, out sat, out val);
+
+
+
+				Color newcolor = ColorFromHSV(hue, sat, val);
+
+				bmp.SetPixel(i % bmp.Width, i / bmp.Width, newcolor);
+
+
+				i++;
+
+			}
+			pictureBox1.Refresh();
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			hue = trackBar1.Value;
+
+		}
+	}
+}
