20573f96dcb9e27f9c858bc1d7b07733160fe172
Dmitry Volodin <mr.molkree@gmail.com>
2018-09-16 01:41:39 +0300

    Lab 2, Task 2 - done Change .NET back to 4.6.1

--- Lab2_task2/Form1.Designer.cs
+++ Lab2_task2/Form1.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -45,11 +57,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(613, 405);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(610, 742);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 33);
+            this.button1.Size = new System.Drawing.Size(131, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Open";
+            this.button1.Text = "Open file";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -80,11 +93,53 @@
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 405);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(440, 300);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(458, 405);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(440, 300);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(904, 405);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(440, 300);
+            this.chart3.TabIndex = 6;
+            this.chart3.Text = "chart3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1358, 450);
+            this.ClientSize = new System.Drawing.Size(1358, 797);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -94,6 +149,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -105,6 +163,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
     }
 }
 
--- Lab2_task2/Form1.cs
+++ Lab2_task2/Form1.cs
@@ -17,57 +17,84 @@ namespace Lab2_task2
             openFileDialog1.ShowDialog();
         }
 
-        byte[] rgbValues;
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        private byte[] getRGBValues(PictureBox pb, out Bitmap bmp, out System.Drawing.Imaging.BitmapData bmpData,
+            out IntPtr ptr, out int bytes)
         {
-            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
-            pictureBox2.Image = Image.FromFile(openFileDialog1.FileName);
-            pictureBox3.Image = Image.FromFile(openFileDialog1.FileName);
-            Bitmap bmp1 = pictureBox1.Image as Bitmap;
-            Bitmap bmp2 = pictureBox2.Image as Bitmap;
-            Bitmap bmp3 = pictureBox3.Image as Bitmap;
+            pb.Image = Image.FromFile(openFileDialog1.FileName);
+            bmp = pb.Image as Bitmap;
 
             // Lock the bitmap's bits.  
-            Rectangle rect = new Rectangle(0, 0, bmp1.Width, bmp1.Height);
-            System.Drawing.Imaging.BitmapData bmpData =
-                bmp1.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bmp1.PixelFormat);
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat);
 
             // Get the address of the first line.
-            IntPtr ptr = bmpData.Scan0;
+            ptr = bmpData.Scan0;
 
             // Declare an array to hold the bytes of the bitmap.
-            int bytes = Math.Abs(bmpData.Stride) * bmp1.Height;
-            rgbValues = new byte[bytes];
+            bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] rgbValues = new byte[bytes];
 
             // Copy the RGB values into the array.
             System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
 
-            //// Set every third value to 255. A 24bpp bitmap will look red.  
-            //for (int counter = 0; counter < rgbValues.Length; counter += 3)
-            //{
-            //    //rgbValues[counter + 1] = 255;                                
-            //}
-            //// Copy the RGB values back to the bitmap
-            //System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            return rgbValues;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Bitmap bmpR, bmpG, bmpB;
+            System.Drawing.Imaging.BitmapData bmpDataR, bmpDataG, bmpDataB;
+            IntPtr ptrR, ptrG, ptrB;
+            int bytes;
+
+            byte[] rgbValuesR = getRGBValues(pictureBox1, out bmpR, out bmpDataR, out ptrR, out bytes);
+            byte[] rgbValuesG = getRGBValues(pictureBox2, out bmpG, out bmpDataG, out ptrG, out bytes);
+            byte[] rgbValuesB = getRGBValues(pictureBox3, out bmpB, out bmpDataB, out ptrB, out bytes);
+
+            // Color histogram
+            int[] red_count = new int[256];
+            int[] green_count = new int[256];
+            int[] blue_count = new int[256];
+
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+            for (int counter = 0; counter < rgbValuesR.Length; counter += 3)
+            {
+                ++red_count[rgbValuesR[counter + 2]];
+                ++green_count[rgbValuesR[counter + 1]];
+                ++blue_count[rgbValuesR[counter]];
+
+                rgbValuesR[counter] = 0;
+                rgbValuesR[counter + 1] = 0;
+                rgbValuesG[counter] = 0;
+                rgbValuesG[counter + 2] = 0;
+                rgbValuesB[counter + 1] = 0;
+                rgbValuesB[counter + 2] = 0;
+
+            }
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValuesR, 0, ptrR, bytes);
+            System.Runtime.InteropServices.Marshal.Copy(rgbValuesG, 0, ptrG, bytes);
+            System.Runtime.InteropServices.Marshal.Copy(rgbValuesB, 0, ptrB, bytes);
 
             // Unlock the bits.
-            bmp1.UnlockBits(bmpData);
+            bmpR.UnlockBits(bmpDataR);
+            bmpG.UnlockBits(bmpDataG);
+            bmpB.UnlockBits(bmpDataB);
 
-            int i = 0;
-            for (int counter = 0; counter < rgbValues.Length; counter += 3)
-            {
-                bmp1.SetPixel(i % bmp1.Width, i / bmp1.Width,
-                    Color.FromArgb(rgbValues[counter + 2], 0, 0));
-                bmp2.SetPixel(i % bmp1.Width, i / bmp1.Width,
-                    Color.FromArgb(0, rgbValues[counter + 1], 0));
-                bmp3.SetPixel(i % bmp1.Width, i / bmp1.Width,
-                    Color.FromArgb(0, 0, rgbValues[counter]));
-                i++;
-            }
             pictureBox1.Refresh();
             pictureBox2.Refresh();
             pictureBox3.Refresh();
+
+            // Show histogram
+            chart1.Series[0].Points.DataBindY(red_count);
+            chart2.Series[0].Points.DataBindY(green_count);
+            chart3.Series[0].Points.DataBindY(blue_count);
+
+            chart1.Series[0].Color = Color.Red;
+            chart2.Series[0].Color = Color.Green;
+            chart3.Series[0].Color = Color.Blue;
         }
     }
 }
