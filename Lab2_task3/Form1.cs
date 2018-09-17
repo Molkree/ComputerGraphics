7c8b118983dfc247cfddcdc1fd5b31dd071bd93a
Taally <shkuro_tanya@mail.ru>
2018-09-17 19:24:57 +0300

    task3 - done

--- Lab2_task3/Form1.cs
+++ Lab2_task3/Form1.cs
@@ -12,9 +12,8 @@ namespace Lab2_task3
 {
 	public partial class Form1 : Form
 	{
-	//	String path;
-		byte[] rgbValues;
-		double pic_hue, pic_sat, pic_val;
+
+        double hue_shift, sat_shift, val_shift;
         Graphics g;
         Bitmap orig_pic;
 
@@ -22,32 +21,19 @@ namespace Lab2_task3
         {
             InitializeComponent();
            
-            pic_hue = trackBar1.Value;
-            pic_sat = trackBar2.Value;
-            pic_val = trackBar3.Value;
-
-
-
+            hue_shift = trackBar1.Value;
+            sat_shift = trackBar2.Value;
+            val_shift = trackBar3.Value;
         }
 
         private void button1_Click(object sender, EventArgs e)
 		{
-				if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-				{
-				//	System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
-				//	sr.Close();
-				//	path = openFileDialog1.FileName;
-				//	pictureBox1.Image(
-				}
+            openFileDialog1.ShowDialog();
 		}
 
-        public static void ColorToHSV(Color color, out double hue, out double sat, out double val)
+        public static void ColorToHSV(int red, int green, int blue, out double hue, out double sat, out double val)
         {
             
-            byte red = color.R;
-            byte green = color.G;
-            byte blue = color.B;
-
             int max = Math.Max(red, Math.Max(green, blue));
             int min = Math.Min(red, Math.Min(green, blue));
 
@@ -74,7 +60,7 @@ namespace Lab2_task3
         }
         
         
-        public static Color ColorFromHSV(double hue, double sat, double val)
+        public static void ColorFromHSV(double hue, double sat, double val, out int red, out int green, out int blue)
 		{
             /* formulas from:
              *  https://en.wikipedia.org/wiki/HSL_and_HSV#From_HSV
@@ -84,20 +70,49 @@ namespace Lab2_task3
             double h1 = hue / 60;
             double x = c * (1 - Math.Abs(h1 % 2 - 1));
             double m = val - c;
-            
+
             if (0 <= h1 && h1 <= 1)
-                return Color.FromArgb(Convert.ToInt32((c + m) * 255), Convert.ToInt32((x + m) * 255), Convert.ToInt32((0 + m) * 255));
+            {
+                red = Convert.ToInt32((c + m) * 255);
+                green = Convert.ToInt32((x + m) * 255);
+                blue = Convert.ToInt32((0 + m) * 255);
+            }
             else if (1 <= h1 && h1 <= 2)
-                return Color.FromArgb(Convert.ToInt32((x + m) * 255), Convert.ToInt32((c + m) * 255), Convert.ToInt32((0 + m) * 255));
+            {
+                red = Convert.ToInt32((x + m) * 255);
+                green = Convert.ToInt32((c + m) * 255);
+                blue = Convert.ToInt32((0 + m) * 255);
+            }
             else if (2 <= h1 && h1 <= 3)
-                return Color.FromArgb(Convert.ToInt32((0 + m) * 255), Convert.ToInt32((c + m) * 255), Convert.ToInt32((x + m) * 255));
+            {
+                red = Convert.ToInt32((0 + m) * 255);
+                green = Convert.ToInt32((c + m) * 255);
+                blue = Convert.ToInt32((x + m) * 255);
+           }
             else if (3 <= h1 && h1 <= 4)
-                return Color.FromArgb(Convert.ToInt32((0 + m) * 255), Convert.ToInt32((x + m) * 255), Convert.ToInt32((c + m) * 255));
+            {
+                red = Convert.ToInt32((0 + m) * 255);
+                green = Convert.ToInt32((x + m) * 255);
+                blue = Convert.ToInt32((c + m) * 255);
+            }
             else if (4 <= h1 && h1 <= 5)
-                return Color.FromArgb(Convert.ToInt32((x + m) * 255), Convert.ToInt32((0 + m) * 255), Convert.ToInt32((c + m) * 255));
+            {
+                red = Convert.ToInt32((x + m) * 255);
+                green = Convert.ToInt32((0 + m) * 255);
+                blue = Convert.ToInt32((c + m) * 255);
+            }
             else if (5 <= h1 && h1 <= 6)
-                return Color.FromArgb(Convert.ToInt32((c + m) * 255), Convert.ToInt32((0 + m) * 255), Convert.ToInt32((x + m) * 255));
-            else return Color.FromArgb(Convert.ToInt32(m), Convert.ToInt32(m), Convert.ToInt32(m));
+            {
+                red = Convert.ToInt32((c + m) * 255);
+                green = Convert.ToInt32((0 + m) * 255);
+                blue = Convert.ToInt32((x + m) * 255);
+            }
+            else
+            {
+                red = Convert.ToInt32(m * 255);
+                green = Convert.ToInt32(m * 255);
+                blue = Convert.ToInt32(m * 255);
+             }
         }
 
         private void showImage()
@@ -107,127 +122,95 @@ namespace Lab2_task3
             
             // Lock the bitmap's bits.  
             Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-
+           
             // get original pixels
-
-			System.Drawing.Imaging.BitmapData origData =
+            System.Drawing.Imaging.BitmapData origData =
 				orig_pic.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
 				orig_pic.PixelFormat);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat);
 
-			// Get the address of the first line.
-			IntPtr ptr = origData.Scan0;
 
-			// Declare an array to hold the bytes of the bitmap.
-			int bytes = Math.Abs(origData.Stride) * orig_pic.Height;
-			rgbValues = new byte[bytes];
+            // Get the address of the first line.
+            IntPtr ptr_orig = origData.Scan0;
+            IntPtr ptr_new = bmpData.Scan0;
+            
+            // Declare an arrays to hold the bytes of the original pic and new bmp.
+            int bytes = Math.Abs(origData.Stride) * orig_pic.Height;
+            byte[] rgbValues_orig = new byte[bytes];
+            byte[] rgbValues_new = new byte[bytes];
 
-			// Copy the RGB values into the array.
-			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-			// Copy the RGB values back to the bitmap
-			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            // Copy the original RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr_orig, rgbValues_orig, 0, bytes);
 
 			// Unlock the bits.
-			orig_pic.UnlockBits(origData);
+			orig_pic.UnlockBits(origData);        
 
-			int i = 0;
-			for (int counter = 0; counter < rgbValues.Length; counter += 3)
+			//int i = 0;
+			for (int counter = 0; counter < rgbValues_orig.Length; counter += 3)
 			{
                 double hue, sat, val;
 
-				Color orig = Color.FromArgb(rgbValues[counter + 2], rgbValues[counter + 1], rgbValues[counter]);
-                // get h, s, v
-                ColorToHSV(orig, out hue, out sat, out val);
+                int red = rgbValues_orig[counter + 2];
+                int green = rgbValues_orig[counter + 1];
+                int blue = rgbValues_orig[counter];
+                ColorToHSV(red, green, blue, out hue, out sat, out val);
 
                 // change h, s, v
-                double new_hue = (hue + pic_hue) % 360;
-                double new_sat = (sat*100 + pic_sat) * 0.01;
+                double new_hue = (hue + hue_shift) % 360;
+                double new_sat = (sat*100 + sat_shift) * 0.01;
                 if (new_sat > 1)
                     new_sat = 1;
                 if (new_sat < 0)
                     new_sat = 0;
 
-                double new_val = (val*100 + pic_val) * 0.01;
+                double new_val = (val*100 + val_shift) * 0.01;
                 if (new_val > 1)
                     new_val = 1;
                 if (new_val < 0)
                     new_val = 0;
 
-				Color newcolor = ColorFromHSV(new_hue, new_sat, new_val);
-                bmp.SetPixel(i % bmp.Width, i / bmp.Width, newcolor);
-				i++;
-			}            
+				ColorFromHSV(new_hue, new_sat, new_val, out red, out green, out blue);
+                rgbValues_new[counter + 2] = Convert.ToByte(red);
+                rgbValues_new[counter + 1] = Convert.ToByte(green);
+                rgbValues_new[counter] = Convert.ToByte(blue);
+			}
+
+            // Copy new RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues_new, 0, ptr_new, bytes);
+            bmp.UnlockBits(bmpData);
             pictureBox1.Refresh();
 		}
 
       
         private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
 		{
-            pic_hue = 0;
-            pic_sat = 0;
-            pic_val = 0;
+            hue_shift = 0;
+            sat_shift = 0;
+            val_shift = 0;
 			pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
             g = Graphics.FromImage(pictureBox1.Image);
-            Bitmap bmp = pictureBox1.Image as Bitmap;
-            
-			// Lock the bitmap's bits.  
-			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-
-			System.Drawing.Imaging.BitmapData bmpData =
-                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bmp.PixelFormat);
-
-			// Get the address of the first line.
-			IntPtr ptr = bmpData.Scan0;
-
-			// Declare an array to hold the bytes of the bitmap.
-			int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
-			rgbValues = new byte[bytes];
-
-			// Copy the RGB values into the array.
-			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-			// Copy the RGB values back to the bitmap
-			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-
-            // Unlock the bits.
-            bmp.UnlockBits(bmpData);
-
-            // save the original picture
-            orig_pic = bmp.Clone() as Bitmap;
-
-            int i = 0;
-			for (int counter = 0; counter < rgbValues.Length; counter += 3)
-			{
-				Color orig = Color.FromArgb(rgbValues[counter + 2], rgbValues[counter + 1], rgbValues[counter]);
-
-                double hue, sat, val;
-                hue = orig.GetHue();
-                sat = orig.GetSaturation();
-                val = orig.GetBrightness();
-
-                ColorToHSV(orig, out hue, out sat, out val);
-          		Color newcolor = ColorFromHSV(hue, sat, val);
-                bmp.SetPixel(i % bmp.Width, i / bmp.Width, newcolor);
-                
-				i++;
-			}
+            orig_pic = pictureBox1.Image.Clone() as Bitmap;
 			pictureBox1.Refresh();
 		}
 
 		private void trackBar1_Scroll(object sender, EventArgs e)
 		{
-			pic_hue = trackBar1.Value;
+			hue_shift = trackBar1.Value;
+            showImage();
 		}
 
         private void trackBar2_Scroll(object sender, EventArgs e)
         {
-            pic_sat = trackBar2.Value;
+            sat_shift = trackBar2.Value;
+            showImage();
         }
 
         private void trackBar3_Scroll(object sender, EventArgs e)
         {
-            pic_val = trackBar3.Value;
+            val_shift = trackBar3.Value;
+            showImage();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -235,49 +218,19 @@ namespace Lab2_task3
             trackBar1.Value = 0;
             trackBar2.Value = 0;
             trackBar3.Value = 0;
-            pic_hue = 0;
-            pic_sat = 0;
-            pic_val = 0;
-            showImage();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
+            hue_shift = 0;
+            sat_shift = 0;
+            val_shift = 0;
             showImage();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-          /*   System.IO.Stream myStream;
-             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-
-             saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-             saveFileDialog1.FilterIndex = 2;
-             saveFileDialog1.RestoreDirectory = true;
-
-             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-             {
-                 if ((myStream = saveFileDialog1.OpenFile()) != null)
-                 {
-                    Image pic = pictureBox1.Image;
-
-                    String path = saveFileDialog1.FileName;
-                    String p1 = path.Substring(0, 3);
-                    p1 = p1.Replace("\\", "//");
-                    String p2 = path.Substring(3);
-                    p2 = p2.Replace("\\", "/");
-                    path = p1 + p2;
-
-                    pic.Save("E://Images/bluefruits.png", System.Drawing.Imaging.ImageFormat.Png);
-                    myStream.Close();
-                 }
-             }
-            */ 
             Image pic = pictureBox1.Image;
             pic.Save("../../newImage.png", System.Drawing.Imaging.ImageFormat.Png);
 
 
-            string message = "Image is saved in the folder 'Lab2_task3'";
+            string message = "Image is saved in the directory 'Lab2_task3'";
             string caption = "Saved";
             MessageBoxButtons buttons = MessageBoxButtons.OK;
             DialogResult result;
