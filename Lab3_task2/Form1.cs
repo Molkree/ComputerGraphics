0dce8d306f9f1cd7a00eb2479c341f3a067c2ad8
Taally <shkuro_tanya@mail.ru>
2018-09-28 18:09:54 +0300

    task2 with mouse click

--- Lab3_task2/Form1.cs
+++ Lab3_task2/Form1.cs
@@ -13,11 +13,10 @@ namespace Lab3_task2
     public partial class Form1 : Form
     {
         Graphics g;
-        Bitmap with_border;
+        Bitmap with_border = null;
+        Color bg_color;
 
-        
-
-        Point find_start_point()
+    /*    Point find_start_point()
         {
             Bitmap bmp = pictureBox1.Image as Bitmap;
 
@@ -39,6 +38,24 @@ namespace Lab3_task2
 
 
             return new Point(0, 0);
+        }*/
+
+        Point find_start_point(Point click)
+        {
+            Bitmap bmp = pictureBox1.Image as Bitmap;
+            
+
+            int act_x = click.X * bmp.Width / pictureBox1.Width;
+            int act_y = click.Y * bmp.Height / pictureBox1.Height;
+
+            bg_color = bmp.GetPixel(act_x, act_y);
+
+            for (int x = act_x; x < bmp.Width; ++x)
+            {
+                if (bmp.GetPixel(x, act_y).ToArgb() != bg_color.ToArgb())
+                    return new Point(x, act_y);
+            }
+            return new Point(-1, -1);
         }
 
         Point new_point(int dir, Point p)
@@ -61,61 +78,72 @@ namespace Lab3_task2
                 return new Point(p.X + 1, p.Y);
         }
 
-        List<Point> find_border()
+        List<Point> find_border(Point click)
         {
             List<Point> points = new List<Point>();
             Bitmap bmp = pictureBox1.Image as Bitmap;
 
-            Point pred_p = find_start_point();
-            points.Add(pred_p);
-            /* label1.Text = pred_p.ToString();
-                    bmp.SetPixel(pred_p.X, pred_p.Y, Color.Red);
-                    pictureBox1.Refresh();
-              */
-
-            
-            int dir = 6;
-            int pred_dir = dir;
-            Point p = pred_p;
-            while (true)
+            Point pred_p = find_start_point(click);
+            if (pred_p.X == -1 || pred_p.Y == -1)
             {
-                dir -= 2;
-                if (dir < 0)
-                    dir += 8;
-                
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show("Неверные координаты", "problem", buttons);
+                points.Clear();
+                return points;
+            }
+            else
+            {
+                points.Add(pred_p);
+                /* label1.Text = pred_p.ToString();
+                        bmp.SetPixel(pred_p.X, pred_p.Y, Color.Red);
+                        pictureBox1.Refresh();
+                  */
+
+
+                int dir = 6;
+                int pred_dir = dir;
+                Point p = pred_p;
                 while (true)
                 {
-                    p = new_point(dir, pred_p);
-                    if (bmp.GetPixel(p.X, p.Y).ToArgb() != Color.White.ToArgb())
+                    dir -= 2;
+                    if (dir < 0)
+                        dir += 8;
+
+                    while (true)
                     {
-                        pred_p = p;
-                        break;
+                        p = new_point(dir, pred_p);
+                        if (bmp.GetPixel(p.X, p.Y).ToArgb() != Color.White.ToArgb())
+                        {
+                            pred_p = p;
+                            break;
+                        }
+                        else
+                            ++dir;
+                        if (dir > 7)
+                            dir -= 8;
                     }
+                    if (dir == (pred_dir + 4) % 8)
+                    {
+                        MessageBoxButtons buttons = MessageBoxButtons.OK;
+                        DialogResult result;
+                        result = MessageBox.Show("Не выделено ни одного пикселя", "problem", buttons);
+                        points.Clear();
+                        return points;
+                    }
+
+                    if (points.Exists(pt => (pt.X == pred_p.X && pt.Y == pred_p.Y)))
+                        break;
                     else
-                        ++dir;
-                    if (dir > 7)
-                        dir -= 8;                        
-                }
-                if (dir == (pred_dir + 4) % 8)
-                {
-                    MessageBoxButtons buttons = MessageBoxButtons.OK;
-                    DialogResult result;
-                    result = MessageBox.Show("Не выделено ни одного пикселя", "problem", buttons);
-                    points.Clear();
-                    return points;
-                }
+                    {
+                        points.Add(pred_p);
+                        //   if (dir == pred_dir )
 
-                if (points.Exists(pt => (pt.X == pred_p.X && pt.Y == pred_p.Y)))
-                    break;
-                else
-                {
-                    points.Add(pred_p);
-                 //   if (dir == pred_dir )
-
-                    pred_dir = dir;
+                        pred_dir = dir;
+                    }
                 }
+                return points;
             }
-            return points;
         }
 
 
@@ -133,26 +161,40 @@ namespace Lab3_task2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
-            List<Point> points = find_border();
+            openFileDialog1.ShowDialog();    
             checkBox1.Checked = false;
-            with_border = pictureBox1.Image.Clone() as Bitmap;
-            foreach (Point pt in points)
-            {
-                with_border.SetPixel(pt.X, pt.Y, Color.Red);
-            }
+            with_border = null;
+            
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (checkBox1.Checked)
             {
+                if (with_border == null)
+                {
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    DialogResult result;
+                    result = MessageBox.Show("Выберите мышкой область внутри границы", "Не выбрана область", buttons);
+                }
+
                 pictureBox1.Image = with_border;
 
             }
             else
             {
                 pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+            }
+        }
+        
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            with_border = null;
+            List<Point> points = find_border(e.Location);
+            with_border = pictureBox1.Image.Clone() as Bitmap;
+            foreach (Point pt in points)
+            {
+                with_border.SetPixel(pt.X, pt.Y, Color.Red);
             }
         }
     }
