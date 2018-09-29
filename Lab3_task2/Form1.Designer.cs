93bf269669e5f949842f6321fc9efc4e8791f9ca
numumotik <numumotik@mail.ru>
2018-09-30 02:48:18 +0300

    Заливка наполовину

--- Lab3_task2/Form1.Designer.cs
+++ Lab3_task2/Form1.Designer.cs
@@ -31,8 +31,10 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +46,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(16, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(179, 42);
             this.button1.TabIndex = 0;
@@ -55,7 +57,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(221, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(662, 682);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -63,27 +65,34 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
-            // checkBox1
+            // button2
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(17, 85);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(146, 21);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "показать границу";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.button2.Location = new System.Drawing.Point(60, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Заливка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(60, 568);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Изменить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 165);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(57, 537);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текущий цвет";
             // 
             // Form1
             // 
@@ -91,10 +100,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(896, 710);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -108,8 +118,10 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- Lab3_task2/Form1.cs
+++ Lab3_task2/Form1.cs
@@ -10,40 +10,59 @@ using System.Windows.Forms;
 
 namespace Lab3_task2
 {
+    enum Mode {Border, Fill };
+
+    public class YXComparer : Comparer<Point>
+    {
+        public override int Compare(Point x, Point y)
+        {
+            if (object.Equals(x, y)) return 0;
+            if (x.Y < y.Y)
+                return -1;
+            if (x.Y == y.Y && x.X < y.X) return -1;
+            return 1;
+        }
+    }
+
     public partial class Form1 : Form
     {
         Graphics g;
         Bitmap with_border = null;
         Color bg_color;
+        Mode mode = Mode.Border;
+        Color fill_color = Color.White;
+        List<Point> points = new List<Point>(); //точки после поиска границы,не отсортированы, чтобы ее нарисовать
+        bool border_finded = false;
+        Image curr = null;
 
-    /*    Point find_start_point()
-        {
-            Bitmap bmp = pictureBox1.Image as Bitmap;
+        /*    Point find_start_point()
+            {
+                Bitmap bmp = pictureBox1.Image as Bitmap;
 
-            for (int x = bmp.Width-1; x >= 0; --x)
-                for (int y = 1; y < bmp.Height; ++y)
-                {
-                    Color c = bmp.GetPixel(x, y);
-                    if (c.ToArgb() != Color.White.ToArgb())
+                for (int x = bmp.Width-1; x >= 0; --x)
+                    for (int y = 1; y < bmp.Height; ++y)
                     {
-                        while (c.ToArgb() != Color.White.ToArgb())
+                        Color c = bmp.GetPixel(x, y);
+                        if (c.ToArgb() != Color.White.ToArgb())
                         {
-                            --x;
-                            c = bmp.GetPixel(x, y);
+                            while (c.ToArgb() != Color.White.ToArgb())
+                            {
+                                --x;
+                                c = bmp.GetPixel(x, y);
+                            }
+                            ++x;
+                            return new Point(x, y);
                         }
-                        ++x;
-                        return new Point(x, y);
                     }
-                }
 
 
-            return new Point(0, 0);
-        }*/
+                return new Point(0, 0);
+            }*/
 
         Point find_start_point(Point click)
         {
-            Bitmap bmp = pictureBox1.Image as Bitmap;
-            
+            //Bitmap bmp = pictureBox1.Image as Bitmap;
+            Bitmap bmp = curr as Bitmap;
 
             int act_x = click.X * bmp.Width / pictureBox1.Width;
             int act_y = click.Y * bmp.Height / pictureBox1.Height;
@@ -78,10 +97,11 @@ namespace Lab3_task2
                 return new Point(p.X + 1, p.Y);
         }
 
-        bool find_border(Point click, out List<Point> points)
+        bool find_border(Point click)
         {
-            points = new List<Point>();
-            Bitmap bmp = pictureBox1.Image as Bitmap;
+            points.Clear();
+            //Bitmap bmp = pictureBox1.Image as Bitmap;
+            Bitmap bmp = curr as Bitmap;
             Point pred_p = find_start_point(click);
             if (pred_p.X == -1 || pred_p.Y == -1)
             {
@@ -94,11 +114,6 @@ namespace Lab3_task2
             else
             {
                 points.Add(pred_p);
-                /* label1.Text = pred_p.ToString();
-                        bmp.SetPixel(pred_p.X, pred_p.Y, Color.Red);
-                        pictureBox1.Refresh();
-                  */
-
                 int cnt_horizontal = 1;
                 Point last_last_p = pred_p;
                 Point last_p = pred_p;
@@ -173,74 +188,141 @@ namespace Lab3_task2
         public Form1()
         {
             InitializeComponent();
+            label1.BackColor = fill_color;
+            colorDialog1.FullOpen = true;
+            colorDialog1.Color = fill_color;
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-            pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
-            g = Graphics.FromImage(pictureBox1.Image);
+            curr = Bitmap.FromFile(openFileDialog1.FileName);
+            pictureBox1.Image = curr;
+            g = Graphics.FromImage(curr);
             pictureBox1.Refresh();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();    
-            checkBox1.Checked = false;
             with_border = null;
-            
+            points.Clear();
+            border_finded = false;
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private List<Point> calc_fill_border()
         {
-          /* if (checkBox1.Checked)
+            List<Point> res = new List<Point>(points);
+            //TODO добавить поиск внутренних областей
+
+
+            res.Sort(new YXComparer());
+            return res;
+        }
+
+        private void time_to_fill(Point[] border)
+        {
+            Pen pen = new Pen(fill_color);
+            for (int i = 0; i < border.Length; i += 2)
             {
-                if (with_border == null)
+                Point first = border[i];
+                Point second = border[i + 1];
+                if (first.X == second.X || first.X == second.X - 1)
+                    continue;
+                first.X = first.X + 1;
+                second.X = second.X - 1;
+                g.DrawLine(pen, first, second);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //if (pictureBox1.Image == null) return;
+            if (curr == null) return;
+            //включен режим поиска границы
+            if (mode == Mode.Border)
+            {
+                //pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = curr;
+                with_border = null;
+                border_finded = find_border(e.Location);
+                //with_border = pictureBox1.Image.Clone() as Bitmap;
+                with_border = curr.Clone() as Bitmap;
+
+                if (border_finded)
+                {
+                    //draw
+                    Point pt = points[0];
+                    using (Graphics gg = Graphics.FromImage(with_border))
+                    {
+                        foreach (Point npt in points)
+                        {
+                            gg.DrawLine(Pens.Red, pt, npt);
+                            pt = npt;
+                        }
+                        gg.DrawLine(Pens.Red, pt, points[0]);
+                    } 
+                    //with_border.SetPixel(pt.X, pt.Y, Color.Red);
+                    pictureBox1.Image = with_border;
+                }
+                else
                 {
                     MessageBoxButtons buttons = MessageBoxButtons.OK;
                     DialogResult result;
                     result = MessageBox.Show("Выберите мышкой область внутри границы", "Не выбрана область", buttons);
                 }
-
-                pictureBox1.Image = with_border;
-
             }
             else
             {
-                pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
-            }*/
-        }
-        
-        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
-        {
-            pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
-            with_border = null;
-            List<Point> points = new List<Point>();
-            bool f = find_border(e.Location, out points);
-            with_border = pictureBox1.Image.Clone() as Bitmap;
-
-            if (f)
-            {
-                //draw
-                Point pt = points[0];
-                using (Graphics g = Graphics.FromImage(with_border))
+                //режим заливки
+                if (border_finded)
                 {
-                    foreach (Point npt in points)
+                    //там красная граница, она мне не нравится, ее надо убрать
+                    //pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+                    Point[] t = points.ToArray();
+                    //костыль
+                    if (t.Length % 2 == 1)
                     {
-                        g.DrawLine(Pens.Red, pt, npt);
-                        pt = npt;
+                        //TODO разобраться с ним наконец
+                        MessageBoxButtons buttons = MessageBoxButtons.OK;
+                        DialogResult result;
+                        result = MessageBox.Show("Пора пилить костыль!", "Error", buttons);
+                    }
+                    else
+                    {
+                        pictureBox1.Image = curr;
+                        List<Point> nborder = calc_fill_border();
+                        time_to_fill(nborder.ToArray());
                     }
                 }
-                g.DrawLine(Pens.Red, pt, points[0]);
-                //with_border.SetPixel(pt.X, pt.Y, Color.Red);
-                pictureBox1.Image = with_border;
+                else
+                {
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    DialogResult result;
+                    result = MessageBox.Show("Не выбрана область. Вернитесь в режим поиска границы", "Error", buttons);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (mode == Mode.Border)
+            {
+                mode = Mode.Fill;
+                button2.Text = "Поиск границы";
             }
             else
             {
-                MessageBoxButtons buttons = MessageBoxButtons.OK;
-                DialogResult result;
-                result = MessageBox.Show("Выберите мышкой область внутри границы", "Не выбрана область", buttons);
+                mode = Mode.Border;
+                button2.Text = "Заливка";
             }
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            fill_color = colorDialog1.Color;
+            label1.BackColor = fill_color;
         }
     }
 }
