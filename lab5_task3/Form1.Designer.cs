f6f09b7355a9a2260768a55fb88c511617bd9343
Taally <shkuro_tanya@mail.ru>
2018-10-11 23:38:19 +0300

    lab5_task3

---
+++ lab5_task3/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace lab5_task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.point1 = new System.Windows.Forms.Label();
+            this.point3 = new System.Windows.Forms.Label();
+            this.point2 = new System.Windows.Forms.Label();
+            this.point0 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(732, 432);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // point1
+            // 
+            this.point1.BackColor = System.Drawing.Color.White;
+            this.point1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point1.ForeColor = System.Drawing.Color.Black;
+            this.point1.Image = ((System.Drawing.Image)(resources.GetObject("point1.Image")));
+            this.point1.Location = new System.Drawing.Point(385, 132);
+            this.point1.Name = "point1";
+            this.point1.Size = new System.Drawing.Size(7, 7);
+            this.point1.TabIndex = 7;
+            this.point1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
+            this.point1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
+            // 
+            // point3
+            // 
+            this.point3.BackColor = System.Drawing.Color.White;
+            this.point3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point3.ForeColor = System.Drawing.Color.Black;
+            this.point3.Image = ((System.Drawing.Image)(resources.GetObject("point3.Image")));
+            this.point3.Location = new System.Drawing.Point(532, 242);
+            this.point3.Name = "point3";
+            this.point3.Size = new System.Drawing.Size(7, 7);
+            this.point3.TabIndex = 8;
+            this.point3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
+            this.point3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
+            // 
+            // point2
+            // 
+            this.point2.BackColor = System.Drawing.Color.White;
+            this.point2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point2.ForeColor = System.Drawing.Color.Black;
+            this.point2.Image = ((System.Drawing.Image)(resources.GetObject("point2.Image")));
+            this.point2.Location = new System.Drawing.Point(473, 116);
+            this.point2.Name = "point2";
+            this.point2.Size = new System.Drawing.Size(7, 7);
+            this.point2.TabIndex = 9;
+            this.point2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
+            this.point2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
+            // 
+            // point0
+            // 
+            this.point0.BackColor = System.Drawing.Color.White;
+            this.point0.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point0.ForeColor = System.Drawing.Color.Black;
+            this.point0.Image = ((System.Drawing.Image)(resources.GetObject("point0.Image")));
+            this.point0.Location = new System.Drawing.Point(345, 242);
+            this.point0.Name = "point0";
+            this.point0.Size = new System.Drawing.Size(7, 7);
+            this.point0.TabIndex = 10;
+            this.point0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
+            this.point0.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 456);
+            this.Controls.Add(this.point0);
+            this.Controls.Add(this.point2);
+            this.Controls.Add(this.point3);
+            this.Controls.Add(this.point1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label point1;
+        private System.Windows.Forms.Label point3;
+        private System.Windows.Forms.Label point2;
+        private System.Windows.Forms.Label point0;
+    }
+}
+
---
+++ lab5_task3/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5_task3
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        float step = 0.05f;
+        Point p;
+        bool isDown;
+
+        // for coordinates
+        int dx, dy;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+
+            // for coordinates
+            dx = pictureBox1.Location.X;
+            dy = pictureBox1.Location.Y;
+            bezier();
+        }
+
+        private Point get_point(Point p)
+        {
+            Point p_new = p;
+            p_new.X -= dx;
+            p_new.Y -= dy;
+            return p_new;
+        }
+
+        private PointF countB(float t)
+        {
+            Point p0 = get_point(point0.Location);
+            Point p1 = get_point(point1.Location);
+            Point p2 = get_point(point2.Location);
+            Point p3 = get_point(point3.Location);
+
+            float pow3 = (float)Math.Pow((1 - t), 3);
+            float pow2 = (float)Math.Pow((1 - t), 2);
+            
+            float bx = p0.X * pow3 + 3 * p1.X * pow2 * t + 3 * p2.X * (1-t)*t*t + p3.X * t*t*t;
+            float by = p0.Y * pow3 + 3 * p1.Y * pow2 * t + 3 * p2.Y * (1 - t) * t * t + p3.Y * t * t * t;
+            return new PointF(bx, by);
+        }
+
+        private void bezier()
+        {
+            g.Clear(Color.White);
+            Pen pen = Pens.Gray;
+            g.DrawLine(pen, get_point(point0.Location), get_point(point1.Location));
+            g.DrawLine(pen, get_point(point1.Location), get_point(point2.Location));
+            g.DrawLine(pen, get_point(point2.Location), get_point(point3.Location));
+
+            List<PointF> curve = new List<PointF>();
+            for (float t = 0; t < 1; t += step)
+                curve.Add(countB(t));
+
+            curve.Add(get_point(point3.Location));
+            g.DrawLines(Pens.Red, curve.ToArray());          
+        }
+
+        private void point_MouseMove(object sender, MouseEventArgs e)
+        {
+            Label l = sender as Label;
+            Size s0 = new Size(e.X - p.X, e.Y - p.Y);
+            if (e.Button == MouseButtons.Left)
+            {
+                l.Location += s0;
+                bezier();
+            }    
+        }
+
+        private void point_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+        }
+
+ 
+    }
+}
