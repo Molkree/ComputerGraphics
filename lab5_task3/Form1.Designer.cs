5abcf077600b4a42e2e203a63cd2ba900ed17756
Taally <shkuro_tanya@mail.ru>
2018-10-13 00:24:06 +0300

    пытаюсь сделать составную кубическую кривую. можно добавлять и удалять точки, рисуется кривая. Но последняя точка улетает куда-то влево.

--- lab5_task3/Form1.Designer.cs
+++ lab5_task3/Form1.Designer.cs
@@ -34,15 +34,17 @@
             this.point3 = new System.Windows.Forms.Label();
             this.point2 = new System.Windows.Forms.Label();
             this.point0 = new System.Windows.Forms.Label();
+            this.add_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(112, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(732, 432);
+            this.pictureBox1.Size = new System.Drawing.Size(632, 432);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -54,8 +56,9 @@
             this.point1.Image = ((System.Drawing.Image)(resources.GetObject("point1.Image")));
             this.point1.Location = new System.Drawing.Point(385, 132);
             this.point1.Name = "point1";
-            this.point1.Size = new System.Drawing.Size(7, 7);
+            this.point1.Size = new System.Drawing.Size(9, 9);
             this.point1.TabIndex = 7;
+            this.point1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.point_MouseClick);
             this.point1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
             this.point1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
             // 
@@ -67,8 +70,9 @@
             this.point3.Image = ((System.Drawing.Image)(resources.GetObject("point3.Image")));
             this.point3.Location = new System.Drawing.Point(532, 242);
             this.point3.Name = "point3";
-            this.point3.Size = new System.Drawing.Size(7, 7);
+            this.point3.Size = new System.Drawing.Size(9, 9);
             this.point3.TabIndex = 8;
+            this.point3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.point_MouseClick);
             this.point3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
             this.point3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
             // 
@@ -80,8 +84,9 @@
             this.point2.Image = ((System.Drawing.Image)(resources.GetObject("point2.Image")));
             this.point2.Location = new System.Drawing.Point(473, 116);
             this.point2.Name = "point2";
-            this.point2.Size = new System.Drawing.Size(7, 7);
+            this.point2.Size = new System.Drawing.Size(9, 9);
             this.point2.TabIndex = 9;
+            this.point2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.point_MouseClick);
             this.point2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
             this.point2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
             // 
@@ -93,16 +98,38 @@
             this.point0.Image = ((System.Drawing.Image)(resources.GetObject("point0.Image")));
             this.point0.Location = new System.Drawing.Point(345, 242);
             this.point0.Name = "point0";
-            this.point0.Size = new System.Drawing.Size(7, 7);
+            this.point0.Size = new System.Drawing.Size(9, 9);
             this.point0.TabIndex = 10;
+            this.point0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.point_MouseClick);
             this.point0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.point_MouseDown);
             this.point0.MouseMove += new System.Windows.Forms.MouseEventHandler(this.point_MouseMove);
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(13, 39);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(75, 37);
+            this.add_button.TabIndex = 11;
+            this.add_button.Text = "Добавить точку";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Щелкните правой кнопкой по точке, чтобы удалить ее";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 456);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.add_button);
             this.Controls.Add(this.point0);
             this.Controls.Add(this.point2);
             this.Controls.Add(this.point3);
@@ -112,6 +139,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -122,6 +150,8 @@
         private System.Windows.Forms.Label point3;
         private System.Windows.Forms.Label point2;
         private System.Windows.Forms.Label point0;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- lab5_task3/Form1.cs
+++ lab5_task3/Form1.cs
@@ -15,10 +15,10 @@ namespace lab5_task3
         Graphics g;
         float step = 0.05f;
         Point p;
-        bool isDown;
 
         // for coordinates
         int dx, dy;
+        List<Label> labels = new List<Label>();
 
         public Form1()
         {
@@ -28,6 +28,12 @@ namespace lab5_task3
             // for coordinates
             dx = pictureBox1.Location.X;
             dy = pictureBox1.Location.Y;
+            label1.Text = "ПКМ по точке,\nчтобы удалить её";
+            labels.Add(point0);
+            labels.Add(point1);
+            labels.Add(point2);
+            labels.Add(point3);
+
             bezier();
         }
 
@@ -39,34 +45,149 @@ namespace lab5_task3
             return p_new;
         }
 
-        private PointF countB(float t)
+        private static long GetBinCoeff(long N, long K)
         {
-            Point p0 = get_point(point0.Location);
-            Point p1 = get_point(point1.Location);
-            Point p2 = get_point(point2.Location);
-            Point p3 = get_point(point3.Location);
+            // This function gets the total number of unique combinations based upon N and K.
+            // N is the total number of items.
+            // K is the size of the group.
+            // Total number of unique combinations = N! / ( K! (N - K)! ).
+            // This function is less efficient, but is more likely to not overflow when N and K are large.
+            // Taken from:  http://blog.plover.com/math/choose.html
+            //
+            long r = 1;
+            long d;
+            if (K > N) return 0;
+            for (d = 1; d <= K; d++)
+            {
+                r *= N--;
+                r /= d;
+            }
+            return r;
+        }
 
-            float pow3 = (float)Math.Pow((1 - t), 3);
-            float pow2 = (float)Math.Pow((1 - t), 2);
-            
-            float bx = p0.X * pow3 + 3 * p1.X * pow2 * t + 3 * p2.X * (1-t)*t*t + p3.X * t*t*t;
-            float by = p0.Y * pow3 + 3 * p1.Y * pow2 * t + 3 * p2.Y * (1 - t) * t * t + p3.Y * t * t * t;
+     /*   private float count_C(int i, int n)
+        {
+            int n_fact = 1;
+            int i_fact = 1;
+            int n_i_fact = 1;
+            for (int k = 1; k < Math.Max(n, i); ++k)
+            {
+                if (k <= n)
+                    n_fact *= k;
+                if (k <= i)
+                    i_fact *= k;
+                if (k <= n - 1)
+                    n_i_fact *= k;
+            }
+            return n_fact / (i_fact * n_i_fact);
+
+        }*/
+
+        private PointF countB(List<Point> pts, float t)
+        {
+            float bx = 0, by = 0;
+            for (int i = 0; i < pts.Count; ++i)
+            {
+                float C = (float)GetBinCoeff(pts.Count, i);
+                float pow1 = (float)Math.Pow(t, i);
+                float pow2 = (float)Math.Pow(1 - t, pts.Count - i);
+                bx += pts[i].X * C * pow1 * pow2;
+                by += pts[i].Y * C * pow1 * pow2;
+            }
+
             return new PointF(bx, by);
+        }
+
+  /*      private PointF countB(List<Point> pts, float t)
+        {
+            float bx, by;
+            if (pts.Count == 2)
+            {
+                bx = pts[0].X * (1 - t) + pts[1].X * t;
+                by = pts[0].Y * (1 - t) + pts[1].Y * t;
+            }
+            else if (pts.Count == 3)
+            {
+                bx = pts[0].X * (1 - t) * (1 - t) + 2 * pts[1].X * (1 - t) * t + pts[2].X * t * t;
+                by = pts[0].Y * (1 - t) * (1 - t) + 2 * pts[1].Y * (1 - t) * t + pts[2].Y * t * t;
+            }
+            else
+            {
+                float pow3 = (float)Math.Pow((1 - t), 3);
+                float pow2 = (float)Math.Pow((1 - t), 2);
+
+                bx = pts[0].X * pow3 + 3 * pts[1].X * pow2 * t + 3 * pts[2].X * (1 - t) * t * t + pts[3].X * t * t * t;
+                by = pts[0].Y * pow3 + 3 * pts[1].Y * pow2 * t + 3 * pts[2].Y * (1 - t) * t * t + pts[3].Y * t * t * t;
+            }
+
+            return new PointF(bx, by);
+        }
+*/
+        bool on_one_line(Point p1, Point p2, Point p3)
+        {
+            // substitute p2 in the equation for line (p1, p3)
+            return ((p1.Y - p3.Y)*p2.X + (p3.X - p1.X)*p2.Y + (p1.X*p3.Y - p3.X*p1.Y) == 0);
+        }
+
+        List<Point> prepare_points()
+        {
+            List<Point> tmp = new List<Point>();
+            // add first two points
+            tmp.Add(get_point(labels[0].Location));
+            tmp.Add(get_point(labels[1].Location));
+
+            for (int i = 2; i < labels.Count;)
+            {
+                // if there are three more points
+                if (i + 2 < labels.Count)
+                {
+                    Point p0 = get_point(labels[i].Location);
+                    Point p1 = get_point(labels[i + 1].Location);
+                    Point p2 = get_point(labels[i + 2].Location);
+
+                    // if this three points aren't in one line => create new point between 1st and 2nd
+                    if (!on_one_line(p0, p1, p2))
+                    {
+                        Point mid = new Point((p0.X + p1.X) / 2, (p0.Y + p1.Y) / 2);
+                        tmp.Add(p0);
+                        tmp.Add(mid);
+                        tmp.Add(p1);
+                        i += 2;
+                    }
+                    else
+                    {
+                        tmp.Add(p0);
+                        tmp.Add(p1);
+                        tmp.Add(p2);
+                        i += 3;
+                    }
+                }
+                else // if there are less then 3 points, just add them
+                {
+                    tmp.Add(get_point(labels[i].Location));
+                    ++i;
+                }
+            }
+
+            return tmp;
         }
 
         private void bezier()
         {
             g.Clear(Color.White);
             Pen pen = Pens.Gray;
-            g.DrawLine(pen, get_point(point0.Location), get_point(point1.Location));
-            g.DrawLine(pen, get_point(point1.Location), get_point(point2.Location));
-            g.DrawLine(pen, get_point(point2.Location), get_point(point3.Location));
+            List<Point> points = prepare_points();
+            
+
+//            g.DrawLine(pen, get_point(point0.Location), get_point(point1.Location));
+ //           g.DrawLine(pen, get_point(point1.Location), get_point(point2.Location));
+  //          g.DrawLine(pen, get_point(point2.Location), get_point(point3.Location));
 
             List<PointF> curve = new List<PointF>();
             for (float t = 0; t < 1; t += step)
-                curve.Add(countB(t));
-
-            curve.Add(get_point(point3.Location));
+                curve.Add(countB(points, t));
+            // what is happening to the last point??? (points[19] for 4 points)
+            curve.Add(points[points.Count - 1]);
             g.DrawLines(Pens.Red, curve.ToArray());          
         }
 
@@ -79,6 +200,49 @@ namespace lab5_task3
                 l.Location += s0;
                 bezier();
             }    
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            Label l = new Label();
+
+            int mid_x = pictureBox1.Width / 2;
+            int mid_y = pictureBox1.Height / 2;
+
+            l.Location = new Point(mid_x, mid_y);
+            l.Name = "point" + (labels.Count - 1).ToString();
+            l.Parent = pictureBox1;
+            Image im = Image.FromFile("../../point.png");
+
+            l.AutoSize = false;
+            l.Image = im;
+            l.Size = im.Size;
+            l.BackColor = Color.White;
+            l.BringToFront();
+            l.MouseClick += point_MouseClick;
+            l.MouseDown += point_MouseDown;
+            l.MouseMove += point_MouseMove;
+            pictureBox1.Controls.Add(l);
+            labels.Add(l);
+            bezier();
+        }
+
+        private void point_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (labels.Count == 2)
+                {
+                    MessageBox.Show("Нужно хотя бы две точки для построения кривой!", "Нельзя удалить", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    Label l = sender as Label;
+                    labels.Remove(l);
+                    l.Dispose();
+                    bezier();
+                }
+            }
         }
 
         private void point_MouseDown(object sender, MouseEventArgs e)
